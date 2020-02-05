e51de0f7ab8a28d60764b86958330fb6b7821351
Jason Venturini <JVenturini>
2020-02-05 13:15:48 -0800

    Simplified to 3 lines until I explore further

--- testSetup.py
+++ testSetup.py
@@ -1,14 +1,4 @@
-nameList = ['Read', 'ColorCorrect', 'Glow', 'Glint']
-xposit = 60 
-nodeList = []
+nameList = ['Read', 'ColorCorrect', 'Glow', 'Glint', 'Viewer']
 
 for i in nameList:
-    x = nuke.createNode(i)
-    # nodeList.append(x)
-
-# for i in nodeList:
-#     i['ypos'].setValue(xposit)
-#     xposit = xposit - 10
-#     print(xposit)
-
-# nodeList[3]
+    x = nuke.createNode(i) 
